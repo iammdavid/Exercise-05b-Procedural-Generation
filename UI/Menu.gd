cc2f1ede2a6555803e7bf5f80904c9a0e1fa38bf
iammdavid <89605918+iammdavid@users.noreply.github.com>
2021-11-02 21:51:37 -0400

    procedural generation done

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -7,3 +7,11 @@ func _on_Restart_pressed():
 
 func _on_Quit_pressed():
 	pass
+extends Control
+
+func _on_Restart_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://Maze/Maze.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
